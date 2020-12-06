9427c6ec1d37d4393bb75c44651f8492befe80f3
Guilherme G. Haetinger <ghaetinger@gmail.com>
2020-12-06 14:04:45 -0300

    🎨 Changed Day 5 structure after Lucas's criticism

--- src/day5.rs
+++ src/day5.rs
@@ -1,4 +1,4 @@
-use {super::util::*, std::collections::HashMap};
+use super::util::*;
 
 fn part1(input_file: &str) -> u64 {
     let seat_ids = get_seat_ids(input_file);
@@ -8,19 +8,15 @@ fn part1(input_file: &str) -> u64 {
 }
 
 fn part2(input_file: &str) -> u64 {
-    let seat_ids = get_seat_ids(input_file);
-    let mut seat_ids_map: HashMap<_, _> = HashMap::new();
-    seat_ids.iter().for_each(|id| {
-        seat_ids_map.insert(*id, *id);
-    });
+    let mut seat_ids = get_seat_ids(input_file);
+    seat_ids.sort_unstable();
+    let mut prev = seat_ids[0];
     let mut res = 0;
-    (7..(126 * 7 - 1)).for_each(|val| {
-        if seat_ids_map.contains_key(&(val - 1))
-            && seat_ids_map.contains_key(&(val + 1))
-            && !seat_ids_map.contains_key(&val)
-        {
-            res = val;
-        }
+    seat_ids[1..].iter().for_each(|id| {
+        if *id - prev == 2 && *id >= 7 && *id < 126 * 7 {
+            res = *id - 1
+        };
+        prev = *id;
     });
     println!("Day 5 (P2) = {}", res);
     res
@@ -31,34 +27,18 @@ fn get_seat_ids(input_file: &str) -> Vec<u64> {
     lines
         .iter()
         .map(|line| {
-            let row = bin_search(&line[..7], 'F', 'B', 0, 127);
-            let col = bin_search(&line[7..], 'L', 'R', 0, 7);
+            let row = bin_search(&line[..7], 'F', 'B');
+            let col = bin_search(&line[7..], 'L', 'R');
             row * 8 + col
         })
         .collect()
 }
 
-fn bin_search(
-    commands: &str,
-    left_command: char,
-    right_command: char,
-    mut min: u64,
-    mut max: u64,
-) -> u64 {
-    let mut final_pos: u64 = 0;
-    commands.chars().for_each(|c| {
-        let offset = (max - min + 1) / 2;
-        if c == right_command {
-            min += offset;
-            final_pos = max;
-        } else if c == left_command {
-            max -= offset;
-            final_pos = min;
-        } else {
-            panic!("No such command: {}!", c);
-        }
-    });
-    final_pos
+fn bin_search(commands: &str, left_command: char, right_command: char) -> u64 {
+    let bin_commands = commands
+        .replace(left_command, "0")
+        .replace(right_command, "1");
+    isize::from_str_radix(&bin_commands, 2).unwrap() as u64
 }
 
 #[cfg(test)]
